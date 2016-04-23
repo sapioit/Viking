b8a30edf0fb91552c6db0a89a9108b24dbda1572
Vladimir Voinea <voineavladimir@gmail.com>
2016-04-23 20:49:46 +0300

    * Cleaner code for handling keep-alive requests

--- src/io/schedulers/io_scheduler.cpp
+++ src/io/schedulers/io_scheduler.cpp
@@ -144,10 +144,8 @@ class scheduler::scheduler_impl {
                 if (!channel->queue) {
                     if (channel->queue.keep_file_open()) {
                         channel->flags &= ~epoll::write;
-                        channel->flags &= epoll::edge_triggered;
                         channel->flags |= epoll::read;
                         callbacks.on_remove(channel);
-                        poll.update(channel);
                     } else {
                         remove(channel);
                         return;
